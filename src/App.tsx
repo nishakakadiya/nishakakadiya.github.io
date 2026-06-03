1e22a82b251c2ff6cf91a6133b65c0377f085210
Nisha <nisha.kakadiya09@gmail.com>
2026-06-03 16:42:46 +1000

    fix:refresh issue

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/nishakakadiya'>
       <div className="app-shell">
         <div className="background-glow" />
         <NavBar />
